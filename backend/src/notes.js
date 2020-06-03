cc4427a5f1d461e641fe0f3fb5dce5f8c4f85ffa
hugokishi <hugopacheco370@gmail.com>
2020-06-02 22:18:53 -0300

    App Mobile da Semana OmniStacks 11

---
+++ backend/src/notes.js
@@ -0,0 +1,35 @@
+/**
+ * Rota / Recurso
+ */
+
+/**
+ * Métodos HTTP - "?" "&"
+ * 
+ * GET: Buscar uma informação do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deleta uma informação no back-end
+ * 
+ */
+
+ /**
+  * Tipos de parâmetros:
+  * 
+  * Query parms: parâmetros nomeados enviados na rota após o "?" e geralmente eles servem para filtros/paginação
+  * Route parms: parâmetros utilizados para identificar recursos
+  * Request Body: corpo da requisição, utilizado para criar ou alterar recursos
+  * 
+  */
+
+/**
+ * Bancos de dados:
+ * 
+ * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
+ * NoSQL: MongoDB, CouchDB, etc
+ * 
+*/
+
+/**
+ * Driver: SELECT * FROM users
+ * Query Builder: table('users').select('*').where()
+ */
